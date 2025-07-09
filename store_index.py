1ddc0a1ef5570609443c909b46d734889d8d4a8f
paloma <valdespaloma15@gmail.com>
2025-07-08 22:26:44 -0500

    updated

---
+++ store_index.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import os
+from src.helper import load_pdf_file, filter_to_minimal_docs, text_split, download_hugging_face_embeddings
+from pinecone import Pinecone
+from pinecone import ServerlessSpec 
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+
+extracted_data=load_pdf_file(data='data/')
+filter_data = filter_to_minimal_docs(extracted_data)
+text_chunks=text_split(filter_data)
+
+embeddings = download_hugging_face_embeddings()
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+index_name = "vitacare"
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings, 
+)
